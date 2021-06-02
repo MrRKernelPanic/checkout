e76a915130176fcd30f0d5a000f3e253b649ff9c
Live session user <mark@mroutledge.co.uk>
2021-06-02 22:01:59 +0100

    Add, Can scan a single item and returns a single price.

---
+++ checkout.py
@@ -0,0 +1,11 @@
+def scan(item_name):
+    if item_name == "Apple":
+        return 0.3
+    elif item_name == "Banana":
+        return 0.25
+    elif item_name == "Steak Pie":
+        return 2.50
+    elif item_name == "Milk":
+        return 1.50
+    else:
+        return 4.75
--- checkout_test.py
+++ checkout_test.py
@@ -1,5 +1,18 @@
 import pytest
 
-def test_helloworld():
-    assert True == False
-    
+from checkout import scan
+
+def test_scan_item_apple():
+    assert scan("Apple") == 0.30
+
+def test_scan_item_banana():
+    assert scan("Banana") == 0.25
+
+def test_scan_item_steak_pie():
+    assert scan("Steak Pie") == 2.50
+
+def test_scan_item_milk():
+    assert scan("Milk") == 1.50
+
+def test_scan_item_washing_powder():
+    assert scan("Washing Powder") == 4.75
