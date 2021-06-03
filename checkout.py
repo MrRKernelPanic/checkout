d7bac597f9a619640e8a1f1f19e0caad8f64ddd0
Live session user <mark@mroutledge.co.uk>
2021-06-03 23:14:46 +0100

    Refactor, Moved pricing into own function.

--- checkout.py
+++ checkout.py
@@ -3,21 +3,8 @@ class Checkout:
         self.basket = {}    
 
     def scan(self, items):
-        running_total = 0
-        
         for item in items:
             self._add_to_basket(item)    
-            if item == "Apple":
-                running_total += 0.3
-            elif item == "Banana":
-                running_total += 0.25
-            elif item == "Steak Pie":
-                running_total += 2.50
-            elif item == "Milk":
-                running_total += 1.50
-            else:
-                running_total += 4.75
-        return running_total
 
     def _add_to_basket(self, item):
         if item in self.basket:
@@ -26,5 +13,19 @@ class Checkout:
             self.basket[item] = 1
         
     def check_basket(self, basket_items):
-        return self.basket.get("Apple")
-        
+        return self.basket.get(basket_items)
+
+    def total_price(self):
+        running_total=0
+        for product, amount in self.basket.items():
+            if product == "Apple":
+                running_total += 0.3 * amount
+            elif product == "Banana":
+                running_total += 0.25 * amount
+            elif product == "Steak Pie":
+                running_total += 2.50 * amount
+            elif product == "Milk":
+                running_total += 1.50 * amount
+            else:
+                running_total += 4.75 * amount
+        return running_total
--- checkout_test.py
+++ checkout_test.py
@@ -2,31 +2,22 @@ import pytest
 
 from checkout import Checkout
 
-def test_scan_item_apple():
+def test_scan_item():
     checkout = Checkout()
-    assert checkout.scan(["Apple"]) == 0.30
-
-def test_scan_item_banana():
-    checkout = Checkout()
-    assert checkout.scan(["Banana"]) == 0.25
-
-def test_scan_item_steak_pie():
-    checkout = Checkout()
-    assert checkout.scan(["Steak Pie"]) == 2.50
-
-def test_scan_item_milk():
-    checkout = Checkout()
-    assert checkout.scan(["Milk"]) == 1.50
-
-def test_scan_item_washing_powder():
-    checkout = Checkout()
-    assert checkout.scan(["Washing Powder"]) == 4.75
+    checkout.scan(["Apple"])
+    assert checkout.basket["Apple"] == 1
 
 def test_scan_multiple_items():
     checkout = Checkout()
-    assert checkout.scan(["Milk", "Apple"]) == 1.80
+    checkout.scan(["Milk", "Apple"])
+    assert checkout.basket["Apple"] == 1 and checkout.basket["Milk"] == 1
 
 def test_check_basket():
     checkout = Checkout()
     checkout.scan(["Apple", "Banana", "Apple"])
     assert checkout.check_basket("Apple") == 2
+
+def test_total_price():
+    checkout = Checkout()
+    checkout.scan(["Apple", "Banana", "Apple"])
+    assert checkout.total_price() == 0.85
